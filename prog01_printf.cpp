c7b3f908eb5f1c7187dd174745b1c7ec2e660616
kennia-sanchez <59614236+kennia-sanchez@users.noreply.github.com>
2021-12-09 14:23:24 -0600

    Add files via upload

---
+++ prog01_printf.cpp
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include <conio.h>
+
+int x;
+
+main()
+{
+	printf("Hola Clase\n");
+	printf("Bienvenidos al nuevo semestre\n");
+	printf("Ser%c con V%cctor Ma%c%cn",160,161,164,162);
+	//%c  á 160
+	// é 130
+	// í 161
+	// ó 162
+	// ú 163
+	// ñ 164
+	// Ñ 165
+	getche();
+}
